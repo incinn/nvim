5a9c1b286da42cfd674419d1650b0871113f2237
barnz <hi@barnz.dev>
2022-11-08 21:08:11 +0000

    install treesitter

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -17,5 +17,7 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use "nvim-treesitter/nvim-treesitter" -- telescope dependency
 end)
 
