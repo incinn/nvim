0cbebe83f2999230a31b8358e1cbe0c4db4b4bde
barnz <hi@barnz.dev>
2022-11-22 19:06:27 +0000

    add telescope keybinds

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,3 +1,9 @@
 local nnoremap = require("user.keymap").nnoremap
 
-nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<leader>pv", "<cmd>Ex<cr>")
+
+-- telescope
+nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
+nnoremap("<leader>fg", "<cmd>Telescope live_grep<cr>")
+nnoremap("<leader>fb", "<cmd>Telescope buffers<cr>")
+nnoremap("<leader>fh", "<cmd>Telescope help_tags<cr>")
